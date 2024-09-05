23e88bb4ec9aaeb8bb1b72b6574f6c6931de90a8
Amit <amitambig@gmail.com>
2024-09-05 05:56:32 +0530

    Adding codes to Repository

---
+++ EKS/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr" {
+  type = string
+}
+variable "public_subnet" {
+  type = list(string)
+}
+variable "private_subnet" {
+  type = list(string)
+}
---
+++ Jenkins Server/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr" {
+  type = string
+}
+variable "public_subnet" {
+  type = list(string)
+}
+variable "instance_type" {
+  
+}
